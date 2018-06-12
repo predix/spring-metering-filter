f78db2106d7ac2eca633659153437d094c4788bf
Pooja-Rajkumar <pooja.rajkumar123@gmail.com>
2018-06-12 13:40:03 -0700

    Fix metering integration test (#13)
    * Fixed metering integration tests.
    
    * added proxies to pom
    
    * Got tests working on bluesso
    
    * removed unnecessary proxies in pom
    
    * fixed spacing issues in files
    
    * more cleanup
    
    * last cleanup hopefully
    
    * Updated README
    
    * fixed all PR issues
    
    * fixed small bug in uaa create instance

---
+++ src/test/java/com/ge/predix/metering/util/Constants.java
@@ -0,0 +1,35 @@
+package com.ge.predix.metering.util;
+
+public class Constants {
+	
+	public static final String SPACE_GUID = "space_guid";
+	public static final String SERVICE_PLAN_GUID = "service_plan_guid";
+	public static final String NAME = "name";
+	public static final String TRUSTED_ISSUER_IDS = "trustedIssuerIds";
+	public static final String PARAMETERS = "parameters";
+	public static final String GUID = "guid";
+	public static final String CREATE_SERVICE_INSTANCE_URL = "/v2/service_instances";
+	public static final String ACCEPTS_INCOMPLETE = "?accepts_incomplete=true";
+	public static final String METADATA = "metadata";
+	public static final String ACS_SERVICE_INSTANCE_NAME = "acs-metering-test";
+	public static final String UAA_SERVICE_INSTANCE_NAME = "uaa-metering-test";
+	public static final String USERNAME = "username";
+	public static final String PASSWORD = "password";
+	public static final String INSTANCE_ID = "instance_id";
+	public static final String ACCESS_TOKEN = "access_token";
+	public static final String BEARER = "bearer ";
+	public static final String SERVICE_COMPONENT_URL = "/v1/service_components/";
+	public static final String PROVIDER = "provider";
+	public static final String CLOUD_FOUNDRY = "cloud-foundry";
+	public static final String ID = "id";
+	public static final String NUREGO_USAGE_URL = "https://api-stg-metering.nurego.com/v1/usages/raw/csv";
+	public static final String UAA_SERVICE_ID = "predix-uaa";
+	public static final String ACS_SERVICE_ID = "predix-acs";
+    public static final String ORGANIZATION_ID = "7141ddb7-cb65-418f-8b38-fe280600ee6d";
+    public static final String ACS_PLAN_ID = "pla_b70d-6248-4a0b-8018-9fc6b9de29e6";
+    public static final String UAA_PLAN_ID = "pla_8f0b-d679-463c-b3d6-977c10414aba";
+	public static final String NUREGO_AUTH_TOKEN_URL = "/v1/auth/token";
+	public static final String UAA_OAUTH_TOKEN = "/oauth/token";
+    public static final String SECRET = "secret";
+
+}
